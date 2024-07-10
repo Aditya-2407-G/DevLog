75d47353c3f55d13748d2dbff7b4f5df03ed4843
Aditya-2407-G <adi.gupta2407@gmail.com>
2024-07-11 02:31:09 +0530

    pages and routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Home, SignIn, SignUp, About, Dashboard, Projects} from './pages/index.js'
 
 function App() {
   return (
-    <div className='text-4xl'>App</div>
+    <BrowserRouter>
+    
+    <Routes>
+
+      <Route path='/' element= {<Home/>}/>
+      <Route path='/signin' element= {<SignIn/>}/>
+      <Route path='/signup' element= {<SignUp/>}/>
+      <Route path='/about' element= {<About/>}/>
+      <Route path='/dashboard' element= {<Dashboard/>}/>
+      <Route path='/projects' element= {<Projects/>}/>
+
+
+    </Routes>
+
+
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+import About from "./About/About.jsx";
+import Dashboard from "./Dashboard/Dashboard.jsx";
+import Home from "./Home/Home.jsx";
+import SignIn from "./SignIn/SignIn.jsx";
+import SignUp from "./SignUp/SignUp.jsx";
+import Projects from "./Projects/Projects.jsx";
+
+
+export  { About, Dashboard, Home, SignIn, SignUp, Projects };
